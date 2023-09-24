f3fb7381c0d97f9e6a550998c3236edf03609ba6
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-09-24 14:13:14 +0100

    general changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import BooksListPage from './pages/books-list';
 import MainLayout from './layouts/main';
+import BooksProvider from './contexts/Books';
 
 function App() {
   return (
     <div className="App">
       <MainLayout>
-        <BooksListPage />
+        <BooksProvider>
+          <BooksListPage />
+        </BooksProvider>
       </MainLayout>
     </div>
   );
