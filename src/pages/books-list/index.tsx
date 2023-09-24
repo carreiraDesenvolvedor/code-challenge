c8fffee5ce820e19af840fdd010ad8d2cbbb9acb
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-09-24 14:11:57 +0100

    Book list updates

--- src/pages/books-list/index.tsx
+++ src/pages/books-list/index.tsx
@@ -1,10 +1,35 @@
 import React, { FC, ReactElement } from 'react';
+import InfiteScroll, {
+  InfiniteScrollItemType,
+} from '../../components/infinite-scrool';
+import './style.css';
+import { useContext } from 'react';
+import {
+  BookContext,
+  IBooksContext,
+} from '../../contexts/Books';
 
 const BooksListPage: FC = (): ReactElement => {
+  const {
+    values: { request },
+  } = useContext(BookContext) as IBooksContext;
   return (
-    <>
-      <h1>Book List Page</h1>
-    </>
+    <section
+      className="book-list-container"
+      aria-label="Books list"
+    >
+      <div className="book-list-container-header">
+        <h1>Books</h1>
+      </div>
+      <div className="book-list-container-list">
+        <InfiteScroll
+          itemType={InfiniteScrollItemType.book}
+          isLoading={request.isLoading}
+          list={request.list}
+          loadingMessage="Fetching books..."
+        />
+      </div>
+    </section>
   );
 };
 
