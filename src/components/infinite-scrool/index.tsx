99b6bee76e63e692baae57bf2424e8f21dbf0a73
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-09-24 12:59:36 +0100

    Created infinite scroll component and tests

--- src/components/infinite-scrool/index.tsx
+++ src/components/infinite-scrool/index.tsx
@@ -1,5 +1,81 @@
-import React, { FC, ReactElement } from 'react';
+import React, {
+  FC,
+  ReactElement,
+  useRef,
+  useEffect,
+} from 'react';
+import { IApiBookItem } from '../../api/books/list';
+import InfiniteScroolBookItem from './items/book';
+import Loading from '../loading';
+import './style.css';
+export enum InfiniteScrollItemType {
+  book = 'book',
+}
 
-const InfiteScroll: FC = (): ReactElement => {
-  return <></>;
+interface IItem {
+  itemType: InfiniteScrollItemType;
+  list: IApiBookItem[];
+}
+
+interface IInfiniteScroll extends IItem {
+  isLoading: boolean;
+  fetchData: () => void;
+  loadingMessage: string;
+}
+
+const InfiteScroll: FC<IInfiniteScroll> = ({
+  itemType,
+  isLoading,
+  fetchData,
+  list,
+  loadingMessage,
+}): ReactElement => {
+  const observerTarget = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchData();
+        }
+      },
+      { threshold: 1 },
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
+  return (
+    <div className="infinite-scroll-container">
+      <div>{getElementByItemType({ itemType, list })}</div>
+      <div ref={observerTarget}></div>
+      {isLoading && (
+        <Loading enabled={true} message={loadingMessage} />
+      )}
+    </div>
+  );
 };
+
+const getElementByItemType = ({
+  itemType,
+  list,
+}: IItem): ReactElement | ReactElement[] => {
+  switch (itemType) {
+    case InfiniteScrollItemType.book:
+      return list.map((item) => (
+        <InfiniteScroolBookItem key={item.id} item={item} />
+      ));
+    default:
+      return <></>;
+  }
+};
+
+export default InfiteScroll;
---
+++ src/components/infinite-scrool/tests/index.test.tsx
@@ -0,0 +1,44 @@
+import {
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
+import InfiteScroll, { InfiniteScrollItemType } from '..';
+
+describe('infinite loading test', () => {
+  test('is loading being rendered when isLoading is true', async () => {
+    render(
+      <InfiteScroll
+        itemType={InfiniteScrollItemType.book}
+        isLoading={true}
+        list={[]}
+        fetchData={() => {}}
+        loadingMessage="Fetching books..."
+      />,
+    );
+
+    //find images
+    const bookImage = screen.getByRole('alert', {
+      name: 'Fetching books...',
+    });
+    expect(bookImage).toBeInTheDocument();
+  });
+
+  test('is loading not being rendered when isLoading is false', async () => {
+    render(
+      <InfiteScroll
+        itemType={InfiniteScrollItemType.book}
+        isLoading={false}
+        list={[]}
+        fetchData={() => {}}
+        loadingMessage="Fetching books..."
+      />,
+    );
+
+    //find images
+    const bookImage = screen.queryByRole('alert', {
+      name: 'Fetching books...',
+    });
+    expect(bookImage).toBeNull();
+  });
+});
