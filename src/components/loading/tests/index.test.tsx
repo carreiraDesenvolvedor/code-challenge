efd3d9cbf0ccd46faf07be42aa5b0859638a9e62
Jonathan Melo <jonathan.melo@workday.com>
2023-10-16 23:32:21 +0100

    Fixed tests for Loading Component

--- src/components/loading/tests/index.test.tsx
+++ src/components/loading/tests/index.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Loading from '..';
 
@@ -5,7 +6,6 @@ describe('test loading', () => {
   test('is loading being rendered correctly when enabled is true', async () => {
     render(<Loading enabled={true} message="Loading" />);
 
-    //find images
     const loading = screen.getByRole('alert', {
       name: 'Loading',
     });
@@ -15,7 +15,6 @@ describe('test loading', () => {
   test('is loading not being rendered correctly when enabled is false', async () => {
     render(<Loading enabled={false} message="Loading" />);
 
-    //find images
     const loading = screen.queryByRole('alert', {
       name: 'Loading',
     });
