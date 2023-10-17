3dc8164647bbf09590f2d99ed79eb556bb966a3a
Jonathan Melo <jonathan.melo@workday.com>
2023-10-17 23:34:57 +0100

    Infinite scroll tests

--- src/components/infinite-scrool/tests/index.test.tsx
+++ src/components/infinite-scrool/tests/index.test.tsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import {
-  fireEvent,
   render,
   screen,
-} from '@testing-library/react';
+} from '../../../test-utils/contexts/books-provider-test';
 import InfiteScroll, { InfiniteScrollItemType } from '..';
 
-describe('infinite loading test', () => {
+describe('infinite loading test render', () => {
   test('is loading being rendered when isLoading is true', async () => {
     render(
       <InfiteScroll
+        currentPage={1}
+        fetchData={()=>{
+          console.log("fetchData...")
+        }}
         itemType={InfiniteScrollItemType.book}
         isLoading={true}
         list={[]}
@@ -17,15 +21,20 @@ describe('infinite loading test', () => {
     );
 
     //find images
-    const bookImage = screen.getByRole('alert', {
+    const loadingElement = screen.getByRole('alert', {
       name: 'Fetching books...',
     });
-    expect(bookImage).toBeInTheDocument();
+    expect(loadingElement).toBeInTheDocument();
+
   });
 
   test('is loading not being rendered when isLoading is false', async () => {
     render(
       <InfiteScroll
+        currentPage={1}
+        fetchData={()=>{
+          console.log("nothing")
+        }}
         itemType={InfiniteScrollItemType.book}
         isLoading={false}
         list={[]}
